1b98cf459605903ecdfc66c0383dec9de01c997a
QZ愤怒 <d17503858090@126.com>
2020-12-28 20:29:52 +0800

    代文龙

---
+++ day11/代文龙/代码/day11_zuoye/src/main/java/com/example/day11_zuoye/HaiBean.java
@@ -0,0 +1,17 @@
+package com.example.day11_zuoye;
+
+public class HaiBean {
+    private Integer image;
+
+    public HaiBean(Integer image) {
+        this.image = image;
+    }
+
+    public Integer getImage() {
+        return image;
+    }
+
+    public void setImage(Integer image) {
+        this.image = image;
+    }
+}
